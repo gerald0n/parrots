bf3d985c236cc0d74b78a24f428b2a036dcba669
Geraldo Gomes <contato.gneto@gmail.com>
2023-04-06 20:56:43 -0300

    adicionado função completa: as cartas viram e fixam se forem iguais; as cartas voltam ao normal se forem diferentes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-const cards = [
+let cards = [
     [
         `<div class="card">
             <div class="back-face face">
@@ -108,11 +108,63 @@ const cards = [
         </div>`
     ]
 ]
-const firstLine = document.querySelector('.line')
 
-for(let i = 0; i < cards.length; i++) {
-  for(let j = 0; j < 2; j++) {
-    firstLine.innerHTML += cards[i][j]
-  }
+const cardsContainer = document.querySelectorAll('.card')
+let cardsSelected = []
+cardsContainer.forEach(card => {
+    card.addEventListener('click', () => {
+        if (cardsSelected.length < 2) {
+            virarCartas(card)
+        }
+    })
+})
+
+function virarCartas(divCurrent) {
+    divCurrent.querySelector('.front-face').style.transform = 'rotateY(-180deg)'
+    divCurrent.querySelector('.back-face').style.transform = 'rotateY(0deg)'
+    cardsSelected.push(divCurrent)
+    if (cardsSelected.length === 2) verificarIgualdade(cardsSelected)
 }
 
+function desvirarCartas() {
+    for (const card of cardsSelected) {
+        card.querySelector('.front-face').style.transform = 'rotateY(0deg)'
+        card.querySelector('.back-face').style.transform = 'rotateY(-180deg)'
+    }
+
+    cardsSelected = []
+}
+
+function verificarFimDoJogo() {
+    const container = Array(document.querySelector('.game-content').children)
+    container.forEach(divList => {
+        if(verify(divList))
+          setTimeout(() => {
+            alert("Você ganhou o jogo!")
+          }, 600);
+    })
+}
+
+function verify(divList) {
+    for (let i = divList.length - 1; i >= 0; i--) {
+        if (!(divList[0].classList.value === divList[i].classList.value)) {
+            return false
+        } return true
+    }
+}
+
+function verificarIgualdade(cardList) {
+    if (
+        cardList[0].querySelector('.back-face img').src !==
+        cardList[1].querySelector('.back-face img').src
+    ) {
+        setTimeout(desvirarCartas, 1000)
+    } else {
+        for (const card of cardList) {
+            card.classList.add('check')
+            card.classList.add('check')
+        }
+        cardsSelected = []
+        verificarFimDoJogo()
+    }
+}

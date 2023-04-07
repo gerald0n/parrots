8c9bd43e090b48a0b12a4132b242e9f050ff8f9a
Geraldo Gomes <contato.gneto@gmail.com>
2023-04-06 23:05:21 -0300

    subindo commit para terceiro teste

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -126,12 +126,18 @@ let cards = [
       </div>`
     ]
 ]
-
-const container = document.querySelector('.game-content')
+let qtdCards = prompt('Com quantas cartas desejar jogar?')
+const container = document.querySelector('.game-content') //container reservado para inserção de das cartas
 let cardsSelected = []
+let contadorDeClicks = 0
 
 function game(qtdDeCartas) {
+    container.innerHTML = ''
+
     cards.sort(comparador)
+    cards.sort(comparador)
+    cards.sort(comparador)
+
     const cartasEmbaralhadas = []
     for (let i = 0; i < qtdDeCartas / 2; i++) {
         for (let j = 0; j < 2; j++) {
@@ -139,6 +145,8 @@ function game(qtdDeCartas) {
         }
     }
 
+    cartasEmbaralhadas.sort(comparador)
+    cartasEmbaralhadas.sort(comparador)
     cartasEmbaralhadas.sort(comparador)
 
     for (let i = 0; i < qtdDeCartas; i++) {
@@ -148,6 +156,7 @@ function game(qtdDeCartas) {
     const cardsContainer = document.querySelectorAll('.card')
     cardsContainer.forEach(card => {
         card.addEventListener('click', () => {
+            contadorDeClicks++
             if (cardsSelected.length < 2) {
                 virarCartas(card)
             }
@@ -180,7 +189,27 @@ function verificarFimDoJogo() {
     container.forEach(divList => {
         if (verificarCartasViradas(divList))
             setTimeout(() => {
-                alert('Você ganhou o jogo!')
+                alert(`Você ganhou o jogo em ${contadorDeClicks} jogadas!`)
+                if (
+                    prompt('Deseja reiniciar o jogo?').toLowerCase() === 'sim'
+                ) {
+                    qtdCards = prompt('Com quantas cartas desejar jogar?')
+                    if (qtdCards != null) {
+                        while (
+                            qtdCards < 4 ||
+                            qtdCards > 14 ||
+                            qtdCards % 2 === 1
+                        ) {
+                            qtdCards = prompt(
+                                'Com quantas cartas desejar jogar?'
+                            )
+                        }
+
+                        game(Number(qtdCards))
+                    }
+                } else {
+                    return
+                }
             }, 600)
     })
 }
@@ -203,14 +232,11 @@ function verificarIgualdade(cardList) {
     } else {
         for (const card of cardList) {
             card.classList.add('check')
-            card.classList.add('check')
-            verificarFimDoJogo()
         }
+        verificarFimDoJogo()
         cardsSelected = []
     }
 }
-
-let qtdCards = prompt('Com quantas cartas desejar jogar?')
 
 if (qtdCards != null) {
     while (qtdCards < 4 || qtdCards > 14 || qtdCards % 2 === 1) {
